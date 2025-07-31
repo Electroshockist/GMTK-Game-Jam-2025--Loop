12489d159b14f1586837a090d9710ec416745951
Electroshockist <ericjohnbalas@gmail.com>
2025-07-31 15:50:42 -0400

    Finishedpsx shader

--- source/game_manager.gd
+++ source/game_manager.gd
@@ -1,1 +1,8 @@
 extends Node
+
+func _ready():
+    pass
+
+func apply_albedo_from_existing(material: ShaderMaterial, from_material: StandardMaterial3D):
+    var tex := from_material.albedo_texture
+    material.set_shader_parameter("albedo_texture", tex)
