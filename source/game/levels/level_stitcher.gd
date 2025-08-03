abfb636ae7b9ad666b744bdb26f18ea019617ce7
Electroshockist <ericjohnbalas@gmail.com>
2025-08-03 11:58:49 -0400

    Updated Level

--- source/game/levels/level_stitcher.gd
+++ source/game/levels/level_stitcher.gd
@@ -1,13 +1,23 @@
 extends Node
 
-var current_level: Node3D
+var _prev_level: Level
+var _current_level: Level
+var _next_level: Level
 
 var _next_loadable_level_id: int = 0
 
-var level_list := [
-	preload("res://source/game/levels/level_01.tscn")
+var _level_list := [
+	preload("res://source/game/levels/level_01_01.tscn"),
+	preload("res://source/game/levels/level_01_02.tscn"),
+	preload("res://source/game/levels/level_01_03.tscn"),
+	preload("res://source/game/levels/level_01_04.tscn"),
+	preload("res://source/game/levels/level_01_05.tscn"),
+	preload("res://source/game/levels/level_01_06.tscn"),
+	preload("res://source/game/levels/level_01_07.tscn"),
+	preload("res://source/game/levels/level_01_08.tscn"),
+	preload("res://source/game/levels/level_01_09.tscn"),
+	preload("res://source/game/levels/level_01_10.tscn"),
 ]
-
 
 func ready():
 	pass
@@ -15,5 +25,13 @@ func ready():
 func load_next_level():
 	pass
 
-func _load_next_level():
-	pass
+func _on_new_level_enter():
+	# close door
+	# swap current level
+	_prev_level.queue_free()
+	_prev_level = _current_level
+	_next_level = null
+
+
+func _on_level_load_trigger():
+	_next_level = _level_list[_next_loadable_level_id]
