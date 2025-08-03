1905cff50dd90af9fdd4f130777871f8b0b0c772
Electroshockist <ericjohnbalas@gmail.com>
2025-08-03 01:30:07 -0400

    Fixed thing

--- source/game_manager.gd
+++ source/game_manager.gd
@@ -4,15 +4,12 @@ signal menu_toggled
 
 var interactor: Interactor
 
-const psx_shader_applicator := preload("res://assets/shaders/psx_shader_applicator.gd")
+# const psx_shader_applicator := preload("res://assets/shaders/psx_shader_applicator.gd")
 
 var character: Character
 
 func _ready():
-	process_mode = Node.PROCESS_MODE_ALWAYS
-
-	_apply_shader_to_all(get_tree().root)
-
+	# _apply_shader_to_all(get_tree().root)
 	menu_toggled.connect(
 		func():
 			character._input_enabled = !get_is_mouse_visible()
@@ -24,11 +21,11 @@ func _input(_event):
 	if Input.is_action_just_pressed("menu"):
 		menu_toggled.emit()
 
-func _apply_shader_to_all(node: Node):
-	for child in node.get_children(true):
-		if (child is MeshInstance3D and child.get_script() == null):
-			child.set_script(psx_shader_applicator)
-		_apply_shader_to_all(child)
+# func _apply_shader_to_all(node: Node):
+# 	for child in node.get_children(true):
+# 		if (child is MeshInstance3D and child.get_script() == null):
+# 			child.set_script(psx_shader_applicator)
+# 		_apply_shader_to_all(child)
 
 func set_mouse_lock(lock: bool):
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED if lock else Input.MOUSE_MODE_VISIBLE
