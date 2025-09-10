afe3f030fa9253a39d07e2f5466e48dff21d6eb1
UmBlood <jennymw1@hotmail.com>
2025-09-10 14:38:21 -0400

    working on anims script, pushing anim list

--- source/game/Character/character_anims.gd
+++ source/game/Character/character_anims.gd
@@ -1,8 +1,17 @@
-extends AnimationPlayer
-	
-func _process(delta: float) -> void:
-	pass
+extends Node3D
 
+@onready var animation_tree : AnimationTree= $"../AnimationTree"
+
+func _process(delta: float) -> void:
+	print(GameManager.character.get_real_velocity().length())
+	animation_tree.set("parameters/conditions/Walking", GameManager.character.get_real_velocity().length() > 0)
+	print(animation_tree.get("parameters/playback").get_current_node())
 
 func _on_character_ready() -> void:
+	animations()
 	print(GameManager.character)
+
+func animations():
+	pass
+	# walk
+	
