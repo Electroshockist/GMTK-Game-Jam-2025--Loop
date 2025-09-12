50acfea05ca415ba08559beb975e01cd3bd219e9
UmBlood <jennymw1@hotmail.com>
2025-09-11 22:41:01 -0400

    script for doorpush, dot in somewhat effect

--- source/game/Character/character_anims.gd
+++ source/game/Character/character_anims.gd
@@ -2,13 +2,18 @@ extends Node3D
 
 @onready var animation_tree : AnimationTree= $"../AnimationTree"
 @onready var animation_player : AnimationPlayer= $"CharacterBody3D/Camera3D/Arms_skeleton_v002/AnimationPlayer"
+@onready var clipboard : BoneAttachment3D = $"..//Camera3D/Arms_skeleton_v002/arms_armature/Skeleton3D/BoneAttachment3D_handL"
+@onready var pencil : BoneAttachment3D = $"..//Camera3D/Arms_skeleton_v002/arms_armature/Skeleton3D/BoneAttachment3D_handR"
 var active_animation
+
 func _process(delta: float) -> void:
 	print(GameManager.character.get_real_velocity().length())
 	print(GameManager.character.is_on_floor())
 	
 	if Input.is_key_pressed(KEY_E):
-		anim_dot()
+		#anim_dot()
+		pass
+		
 	else:
 		animation_tree.set("parameters/conditions/Idle", GameManager.character.is_on_floor() == true and GameManager.character.get_real_velocity().length() <= 1.0 )
 		animation_tree.set("parameters/conditions/Walking", GameManager.character.is_on_floor() == true and GameManager.character.get_real_velocity().length() > 1.0 )
@@ -19,7 +24,12 @@ func _process(delta: float) -> void:
 	print(animation_tree.get("parameters/conditions/Walking"))
 	print(GameManager.character.get_real_velocity().length())
 	print(GameManager.character.get_real_velocity())'''
-
+func _ready() :
+	print(clipboard)
+	print(pencil)
+	clipboard.hide()
+	pencil.hide()
+	
 func _on_character_ready() -> void:
 	animations()
 	print('starting animations')
@@ -28,18 +38,26 @@ func animations():
 	print(GameManager.character)
 	
 func anim_door_open():
-	animation_tree.travel("arms_anim_doorpush")
+	var state_machine = animation_tree["parameters/playback"]
+	state_machine.travel("arms_anim_doorpush")
+	await get_tree().create_timer(.7).timeout
+	state_machine.travel('arms_anim_loop_idle')
 	
 func anim_dot():
+	clipboard.show()
+	pencil.show()
 	var state_machine = animation_tree["parameters/playback"]
 	active_animation = state_machine
 	state_machine.travel("arms_anim_dot")
 	print('dot anim?')
 	await get_tree().create_timer(1).timeout
+	clipboard.hide()
+	pencil.hide()
 	state_machine.travel('arms_anim_loop_idle')
 	'''
 	_on_AnimationPlayer_animation_finished()
 	print('animation finished signal')'''
+	
 	
 func anim_jot():
 	animation_tree.travel("arms_anim_jot")
--- source/game/levels/Props/door.gd
+++ source/game/levels/Props/door.gd
@@ -3,6 +3,8 @@ extends Interactable
 class_name Door
 
 @export var anim_player: AnimationPlayer
+@onready var character_anims : Node3D = $/source/game/Character/character_anims
+@onready var animation_tree : AnimationTree= $"../CharacterBody3D/AnimationTree"
 
 func _ready() -> void:
 	super._ready()
@@ -14,4 +16,9 @@ func _ready() -> void:
 	)
 
 func on_interact():
+	var state_machine = animation_tree["parameters/playback"]
+	state_machine.travel('arms_anim_doorpush')
+	await get_tree().create_timer(.3).timeout
 	anim_player.play("Door_Open")
+	await get_tree().create_timer(.7).timeout
+	state_machine.travel('arms_anim_loop_idle')
